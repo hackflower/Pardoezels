7f1455a44e18b447ffa0434da71d8cfc67780218
Kai <kaitholen@outlook.com>
2026-02-10 13:10:43 +0100

    added template

---
+++ Service/ITaskService.cs
@@ -0,0 +1,7 @@
+public interface ITaskService
+{
+    IEnumerable<TaskItem> GetAllTasks();
+    void AddTask(string description);
+    void RemoveTask(int id);
+    void ToggleTaskCompletion(int id);
+}
