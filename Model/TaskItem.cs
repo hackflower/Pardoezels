2c821603cb4846a12505d7bf1fb6661e9bd1231e
Kai <kaitholen@outlook.com>
2026-02-24 13:29:46 +0100

    tostring

--- Model/TaskItem.cs
+++ Model/TaskItem.cs
@@ -3,4 +3,9 @@ public class TaskItem
     public int Id { get; set; }
     public required string Description { get; set; }
     public bool Completed { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}.\nTask Description: {Description},\nCompleted: {Completed}\n";
+    }
 }
