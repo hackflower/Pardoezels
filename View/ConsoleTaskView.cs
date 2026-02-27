8b6d7b70dd1b312433806328e8192b171e5b25f3
Stephanie <stephaniepoleij@gmail.com>
2026-02-27 11:00:47 +0100

    index switch en priority toevoegen

--- View/ConsoleTaskView.cs
+++ View/ConsoleTaskView.cs
@@ -18,42 +18,42 @@ public class ConsoleTaskView : ITaskView
         return Console.ReadLine();
     }
     int ShowMenu(string title, string[] options)
-{
-    int selectedIndex = 0;
-    ConsoleKey key;
-
-    do
     {
-        Console.Clear();
-        Console.WriteLine(title);
-        Console.WriteLine();
+        int selectedIndex = 0;
+        ConsoleKey key;
 
-        for (int i = 0; i < options.Length; i++)
+        do
         {
-            if (i == selectedIndex)
+            Console.Clear();
+            Console.WriteLine(title);
+            Console.WriteLine();
+
+            for (int i = 0; i < options.Length; i++)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("> " + options[i]);
-                Console.ResetColor();
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("> " + options[i]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("  " + options[i]);
+                }
             }
-            else
-            {
-                Console.WriteLine("  " + options[i]);
-            }
-        }
 
-        key = Console.ReadKey(true).Key;
+            key = Console.ReadKey(true).Key;
 
-        if (key == ConsoleKey.UpArrow && selectedIndex > 0)
-            selectedIndex--;
+            if (key == ConsoleKey.UpArrow && selectedIndex > 0)
+                selectedIndex--;
 
-        if (key == ConsoleKey.DownArrow && selectedIndex < options.Length - 1)
-            selectedIndex++;
+            if (key == ConsoleKey.DownArrow && selectedIndex < options.Length - 1)
+                selectedIndex++;
 
-    } while (key != ConsoleKey.Enter);
+        } while (key != ConsoleKey.Enter);
 
-    return selectedIndex;
-}
+        return selectedIndex;
+    }
     public void Run()
     {
         while (true)
@@ -105,3 +105,4 @@ public class ConsoleTaskView : ITaskView
         }
     }
 }
+// test
