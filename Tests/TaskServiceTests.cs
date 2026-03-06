ceef31d3ccd120fda359a3e67036c3f7d12424a8
Kai <kaitholen@outlook.com>
2026-03-06 12:32:36 +0100

    ja

---
+++ Tests/TaskServiceTests.cs
@@ -0,0 +1,32 @@
+﻿
+using Xunit;
+namespace Tests;
+
+public class TaskServiceTests
+{
+    [Fact]
+    public void RemoveTaskTest()
+    {
+        string filePath = "tasks.json";
+
+        ITaskRepository repository = new JsonTaskRepository(filePath);
+        ITaskService service = new TaskService(repository);
+
+        Efteldingen<TaskItem> tasks = new Efteldingen<TaskItem>();
+
+        var testTask = new TaskItem
+        {
+            Id = 1,
+            Name = "test",
+            Description =
+            "test",
+            CreatedAt = DateTime.Now,
+            Completed = false
+        };
+        tasks[0] = testTask;
+
+        service.RemoveTask(1);
+
+        Assert.Equal(0, tasks.Count);
+    }
+}
