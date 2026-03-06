beed3fe46942a58e0c2db3b73ab6f6fbe83353c7
YouriSchmitz <youri.schmitz@gmail.com>
2026-03-06 12:02:43 +0100

    filter en sort

--- Model/IEfteldingen.cs
+++ Model/IEfteldingen.cs
@@ -3,4 +3,8 @@ interface IEfteldingen<T>
     void Add(T task);
     void Remove(T task);
     T? Find<K>(K key, Func<T, K, bool> comparer);
+    Efteldingen<T> Filter(Func<T, bool> predicate);
+    void Sort(Comparison<T> comparison);
+    int Count { get; }
+    IEnumerator<T> GetEnumerator();
 }
